d2374f170e9c0e2b8c861dc6c99c08a89018f079
lucalain <luca.lain@mail.polimi.it>
2024-04-04 12:56:06 +0200

    Update user profile page

---
+++ dima_project/lib/widgets/home/binaryoption_widget.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomBinaryOption extends StatefulWidget {
+  final String textLeft;
+  final String textRight;
+
+  const CustomBinaryOption({
+    required this.textLeft,
+    required this.textRight,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _CustomBinaryOptionState createState() => _CustomBinaryOptionState();
+}
+
+class _CustomBinaryOptionState extends State<CustomBinaryOption> {
+  bool lr = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: CupertinoTheme(
+        data: const CupertinoThemeData(
+          primaryColor: CupertinoColors.activeBlue,
+        ),
+        child: Container(
+          color: CupertinoColors.white,
+          height: 50,
+          child: Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      lr = false;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.textLeft,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: lr == false
+                              ? CupertinoTheme.of(context).primaryColor
+                              : CupertinoColors.inactiveGray,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        height: lr == false ? 3 : 1,
+                        color: lr == false
+                            ? CupertinoTheme.of(context).primaryColor
+                            : CupertinoColors.opaqueSeparator,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      lr = true;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.textRight,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: lr == true
+                              ? CupertinoTheme.of(context).primaryColor
+                              : CupertinoColors.inactiveGray,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        height: lr == true ? 3 : 1,
+                        color: lr == true
+                            ? CupertinoTheme.of(context).primaryColor
+                            : CupertinoColors.opaqueSeparator,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- dima_project/lib/widgets/home/userprofile_widget.dart
+++ dima_project/lib/widgets/home/userprofile_widget.dart
@@ -1,168 +1,192 @@
 import 'package:dima_project/models/categories.dart';
 import 'package:dima_project/models/user.dart';
 import 'package:dima_project/services/auth_service.dart';
+import 'package:dima_project/widgets/home/binaryoption_widget.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 class UserProfile extends StatelessWidget {
   final UserData user;
-
-  const UserProfile({Key? key, required this.user}) : super(key: key);
+  final bool isMyProfile;
+  const UserProfile({super.key, required this.user, this.isMyProfile = true});
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text(
-          'Profile',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: CupertinoColors.systemPink,
-          ),
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: CupertinoColors.white,
+        leading: isMyProfile
+            ? GestureDetector(
+                onTap: () {},
+                child: const Icon(CupertinoIcons.settings,
+                    color: CupertinoColors.black),
+              )
+            : const SizedBox(),
+        trailing: GestureDetector(
+          onTap: () {},
+          child: const Icon(CupertinoIcons.share, color: CupertinoColors.black),
         ),
       ),
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              ClipOval(
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  color: CupertinoColors.lightBackgroundGray,
-                  child: user.imagePath != null
-                      ? Image.memory(
-                          user.imagePath!,
-                          fit: BoxFit.cover,
-                        )
-                      : const Icon(
-                          CupertinoIcons.photo,
-                          size: 50,
-                          color: CupertinoColors.systemGrey,
-                        ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                '${user.name} ${user.surname}',
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: CupertinoColors.black,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                user.email,
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: CupertinoColors.black,
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Interests:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: CupertinoColors.black,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: user.categories
-                    .map(
-                      (category) => Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4),
-                        child: Row(
-                          children: [
-                            Icon(
-                              CategoryIconMapper.iconForCategory(category),
-                              size: 24,
-                              color: CupertinoColors.black,
-                            ),
-                            const SizedBox(width: 8),
-                            Text(
-                              category,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: CupertinoColors.black,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 8),
+            Container(
+              color: CupertinoColors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 55),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipOval(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        color: CupertinoColors.lightBackgroundGray,
+                        child: user.imagePath != null
+                            ? Image.memory(
+                                user.imagePath!,
+                                fit: BoxFit.cover,
+                              )
+                            : const Icon(
+                                CupertinoIcons.photo,
+                                size: 50,
+                                color: CupertinoColors.systemGrey,
                               ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        user.username,
+                        style: CupertinoTheme.of(context).textTheme.textStyle,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        '${user.name} ${user.surname}',
+                        style: CupertinoTheme.of(context).textTheme.textStyle,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: user.categories
+                          .map((category) => Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 4),
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      CategoryIconMapper.iconForCategory(
+                                          category),
+                                      size: 24,
+                                      color: CupertinoColors.black,
+                                    ),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      category,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        color: CupertinoColors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ))
+                          .toList(),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Text("789",
+                                style: CupertinoTheme.of(context)
+                                    .textTheme
+                                    .textStyle),
+                            const SizedBox(height: 10),
+                            Text(
+                              "Groups",
+                              style: CupertinoTheme.of(context)
+                                  .textTheme
+                                  .textStyle
+                                  .copyWith(color: CupertinoColors.systemGrey),
                             ),
                           ],
                         ),
-                      ),
-                    )
-                    .toList(),
-              ),
-              const SizedBox(height: 20),
-              const SizedBox(height: 20),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        'Edit Profile',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: CupertinoColors.black,
+                        Column(
+                          children: [
+                            Text("1.2000",
+                                style: CupertinoTheme.of(context)
+                                    .textTheme
+                                    .textStyle),
+                            const SizedBox(height: 10),
+                            Text(
+                              "Followers",
+                              style: CupertinoTheme.of(context)
+                                  .textTheme
+                                  .textStyle
+                                  .copyWith(color: CupertinoColors.systemGrey),
+                            ),
+                          ],
                         ),
-                      ),
-                      SizedBox(width: 8),
-                      Icon(
-                        CupertinoIcons.pencil,
-                        color: CupertinoColors.systemGrey,
-                      ),
-                    ],
-                  ),
-                  CupertinoButton(
-                    onPressed: null,
-                    child: Icon(
-                      CupertinoIcons.arrow_right,
-                      color: CupertinoColors.systemGrey,
+                        Column(
+                          children: [
+                            Text("789",
+                                style: CupertinoTheme.of(context)
+                                    .textTheme
+                                    .textStyle),
+                            const SizedBox(height: 10),
+                            Text(
+                              "Following",
+                              style: CupertinoTheme.of(context)
+                                  .textTheme
+                                  .textStyle
+                                  .copyWith(color: CupertinoColors.systemGrey),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
+                    const SizedBox(height: 20),
+                    !isMyProfile
+                        ? CupertinoButton.filled(
+                            onPressed: () {},
+                            child: const Text(
+                              'Follow',
+                            ),
+                          )
+                        : const SizedBox(),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              color: CupertinoColors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: const Column(
+                children: [
+                  CustomBinaryOption(
+                    textLeft: 'Events created',
+                    textRight: 'Events joined',
                   ),
+                  /*GridView.count(
+                    physics: const NeverScrollableScrollPhysics(),
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    childAspectRatio: 1 / 1.5,
+                    children: List.generate(5, (index) => const Placeholder()),
+                  ),*/
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Row(
-                    children: [
-                      Text(
-                        'Logout',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: CupertinoColors.black,
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Icon(
-                        FontAwesomeIcons.signOutAlt,
-                        color: CupertinoColors.systemGrey,
-                      ),
-                    ],
-                  ),
-                  CupertinoButton(
-                    onPressed: () => _signOut(context),
-                    child: const Icon(
-                      CupertinoIcons.arrow_right,
-                      color: CupertinoColors.systemGrey,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
