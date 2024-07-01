73c5bee35d27b370a3c6ce702638b311d099f9ab
lucalain <luca.lain@mail.polimi.it>
2024-07-01 10:27:06 +0200

    Fix groups stream

--- dima_project/lib/widgets/home/user_profile/show_groups_page.dart
+++ dima_project/lib/widgets/home/user_profile/show_groups_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dima_project/models/group.dart';
 import 'package:dima_project/models/user.dart';
 import 'package:dima_project/services/database_service.dart';
@@ -18,8 +21,11 @@ class ShowGroupsPage extends StatefulWidget {
 }
 
 class ShowGroupsPageState extends State<ShowGroupsPage> {
-  late Stream<List<Group>> groupsStream;
-
+  late StreamSubscription<List<QueryDocumentSnapshot<Map<String, dynamic>>>>?
+      _groupsStreamSubscription;
+  late final StreamController<List<QueryDocumentSnapshot<Map<String, dynamic>>>>
+      _groupsStreamController =
+      StreamController<List<QueryDocumentSnapshot<Map<String, dynamic>>>>();
   @override
   void initState() {
     super.initState();
@@ -27,7 +33,11 @@ class ShowGroupsPageState extends State<ShowGroupsPage> {
   }
 
   init() {
-    groupsStream = DatabaseService.getGroupsStream(widget.user.uuid!);
+    _groupsStreamSubscription =
+        DatabaseService.getGroupsStreamUser(widget.user.uuid!)
+            .listen((snapshot) {
+      _groupsStreamController.add(snapshot);
+    });
   }
 
   @override
@@ -43,26 +53,29 @@ class ShowGroupsPageState extends State<ShowGroupsPage> {
         ),
         middle: const Text('Groups'),
       ),
-      child: StreamBuilder<List<Group>>(
-        stream: groupsStream,
+      child: StreamBuilder<List<QueryDocumentSnapshot<Map<String, dynamic>>>>(
+        stream: _groupsStreamController.stream,
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return Center(
               child: Text('Error: ${snapshot.error}'),
             );
           }
-          final docs = snapshot.data ?? [];
-          if (docs.isEmpty &&
-              snapshot.connectionState == ConnectionState.active) {
+          if (!snapshot.hasData) {
+            return const CupertinoActivityIndicator();
+          }
+
+          final docs = snapshot.data;
+          if (docs == null || docs.isEmpty) {
             return const Center(
-              child: Text("No groups found"),
+              child: Text('No groups'),
             );
           }
 
           return ListView.builder(
             itemCount: docs.length,
             itemBuilder: (context, index) {
-              final group = docs[index];
+              final group = Group.fromSnapshot(docs[index]);
               return FutureBuilder(
                   future: DatabaseService.getUserData(group.admin!),
                   builder: (context, snapshot) {
@@ -88,5 +101,12 @@ class ShowGroupsPageState extends State<ShowGroupsPage> {
         },
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _groupsStreamSubscription?.cancel();
+    _groupsStreamController.close();
+    super.dispose();
   }
 }
