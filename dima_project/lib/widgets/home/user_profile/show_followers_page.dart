659dd6efe12f3127e43d99d224ec2b18c55e2db7
lucalain <luca.lain@mail.polimi.it>
2024-07-09 12:41:11 +0200

    Add search bar to groups, followers and following pages

--- dima_project/lib/widgets/home/user_profile/show_followers_page.dart
+++ dima_project/lib/widgets/home/user_profile/show_followers_page.dart
@@ -27,6 +27,9 @@ class ShowFollowersState extends State<ShowFollowers> {
   late final StreamController<DocumentSnapshot<Map<String, dynamic>>>
       _followersStreamController =
       StreamController<DocumentSnapshot<Map<String, dynamic>>>();
+  String _searchText = '';
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -55,83 +58,116 @@ class ShowFollowersState extends State<ShowFollowers> {
             ? const Text('Followers')
             : const Text('Following'),
       ),
-      child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-        stream: _followersStreamController.stream,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          }
-          if (!snapshot.hasData || snapshot.data!.data() == null) {
-            return const CupertinoActivityIndicator();
-          }
-          if (widget.followers &&
-              snapshot.data!.data()!["followers"].length == 0) {
-            return const Center(
-              child: Text('No followers'),
-            );
-          }
-          if (!widget.followers &&
-              snapshot.data!.data()!["following"].length == 0) {
-            return const Center(
-              child: Text('Not following anyone'),
-            );
-          }
-          return ListView.builder(
-            itemCount: widget.followers
-                ? snapshot.data!.data()!["followers"].length
-                : snapshot.data!.data()!["following"].length,
-            itemBuilder: (context, index) {
-              final followers = snapshot.data!.data()!["followers"];
-              final following = snapshot.data!.data()!["following"];
+      child: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              height: 50,
+              child: CupertinoTextField(
+                controller: _searchController,
+                onChanged: (_) => (setState(() {
+                  _searchText = _searchController.text;
+                })),
+                prefix: const Icon(CupertinoIcons.search,
+                    color: CupertinoColors.systemPink),
+                decoration: BoxDecoration(border: Border.all(width: 0)),
+              ),
+            ),
+            StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              stream: _followersStreamController.stream,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                }
+                if (!snapshot.hasData || snapshot.data!.data() == null) {
+                  return const CupertinoActivityIndicator();
+                }
+                if (widget.followers &&
+                    snapshot.data!.data()!["followers"].length == 0) {
+                  return const Center(
+                    child: Text('No followers'),
+                  );
+                }
+                if (!widget.followers &&
+                    snapshot.data!.data()!["following"].length == 0) {
+                  return const Center(
+                    child: Text('Not following anyone'),
+                  );
+                }
+                int i = 0;
+                final docs = widget.followers
+                    ? snapshot.data!.data()!["followers"]
+                    : snapshot.data!.data()!["following"];
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: docs.length,
+                  itemBuilder: (context, index) {
+                    final List<dynamic> uuids = docs;
 
-              final List<dynamic> uuids =
-                  widget.followers ? followers : following;
+                    if (uuids.isEmpty) {
+                      return Center(
+                        child: widget.followers
+                            ? const Text('No followers')
+                            : const Text('Not following anyone'),
+                      );
+                    }
 
-              if (uuids.isEmpty) {
-                return Center(
-                  child: widget.followers
-                      ? const Text('No followers')
-                      : const Text('Not following anyone'),
-                );
-              }
+                    final String uid = uuids[index].toString();
 
-              final String uid = uuids[index].toString();
-
-              return StreamBuilder<UserData>(
-                stream: DatabaseService.getUserDataFromUUID(uid.toString()),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CupertinoActivityIndicator(); // Or any loading indicator
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else {
-                    final UserData userData = snapshot.data!;
-                    return StreamBuilder(
-                        stream: DatabaseService.isFollowing(
-                            userData.uuid!, widget.uuid),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const CupertinoActivityIndicator();
-                          } else if (snapshot.hasError) {
-                            return Text('Error: ${snapshot.error}');
-                          } else {
-                            final isFollowing = snapshot.data as int;
-                            return UserTile(
-                              user: userData,
-                              uuid: widget.uuid,
-                              isFollowing: isFollowing,
-                            );
+                    return StreamBuilder<UserData>(
+                      stream:
+                          DatabaseService.getUserDataFromUUID(uid.toString()),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CupertinoActivityIndicator(); // Or any loading indicator
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          final UserData userData = snapshot.data!;
+                          if (!RegExp(_searchText, caseSensitive: false)
+                              .hasMatch(userData.username)) {
+                            i += 1;
+                            if (i == docs.length) {
+                              return Center(
+                                child: widget.followers
+                                    ? const Text('No followers')
+                                    : const Text('Not following anyone'),
+                              );
+                            }
+                            return const SizedBox.shrink();
                           }
-                        });
-                  }
-                },
-              );
-            },
-          );
-        },
+
+                          return StreamBuilder(
+                              stream: DatabaseService.isFollowing(
+                                  userData.uuid!, widget.uuid),
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState ==
+                                    ConnectionState.waiting) {
+                                  return const CupertinoActivityIndicator();
+                                } else if (snapshot.hasError) {
+                                  return Text('Error: ${snapshot.error}');
+                                } else {
+                                  final isFollowing = snapshot.data as int;
+                                  return UserTile(
+                                    user: userData,
+                                    uuid: widget.uuid,
+                                    isFollowing: isFollowing,
+                                  );
+                                }
+                              });
+                        }
+                      },
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- dima_project/lib/widgets/home/user_profile/show_groups_page.dart
+++ dima_project/lib/widgets/home/user_profile/show_groups_page.dart
@@ -25,6 +25,8 @@ class ShowGroupsPageState extends State<ShowGroupsPage> {
   late final StreamController<List<QueryDocumentSnapshot<Map<String, dynamic>>>>
       _groupsStreamController =
       StreamController<List<QueryDocumentSnapshot<Map<String, dynamic>>>>();
+  final TextEditingController _searchController = TextEditingController();
+  String _searchText = '';
   @override
   void initState() {
     super.initState();
@@ -51,52 +53,81 @@ class ShowGroupsPageState extends State<ShowGroupsPage> {
         ),
         middle: const Text('Groups'),
       ),
-      child: StreamBuilder<List<QueryDocumentSnapshot<Map<String, dynamic>>>>(
-        stream: _groupsStreamController.stream,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          }
-          if (!snapshot.hasData) {
-            return const CupertinoActivityIndicator();
-          }
+      child: SafeArea(
+        child: Column(children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            height: 50,
+            child: CupertinoTextField(
+              controller: _searchController,
+              onChanged: (_) => (setState(() {
+                _searchText = _searchController.text;
+              })),
+              prefix: const Icon(CupertinoIcons.search,
+                  color: CupertinoColors.systemPink),
+              decoration: BoxDecoration(border: Border.all(width: 0)),
+            ),
+          ),
+          StreamBuilder<List<QueryDocumentSnapshot<Map<String, dynamic>>>>(
+            stream: _groupsStreamController.stream,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error: ${snapshot.error}'),
+                );
+              }
+              if (!snapshot.hasData) {
+                return const CupertinoActivityIndicator();
+              }
 
-          final docs = snapshot.data;
-          if (docs == null || docs.isEmpty) {
-            return const Center(
-              child: Text('No groups'),
-            );
-          }
-
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              final group = Group.fromSnapshot(docs[index]);
-              return FutureBuilder(
-                  future: DatabaseService.getUserData(group.admin!),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const CupertinoActivityIndicator();
-                    } else if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else {
-                      final admin = snapshot.data!.username;
-                      group.admin = admin;
-                      return GroupTile(
-                          uuid: widget.uuid,
-                          group: group,
-                          isJoined: group.members!.contains(widget.uuid)
-                              ? 1
-                              : group.requests!.contains(widget.uuid)
-                                  ? 2
-                                  : 0);
+              final docs = snapshot.data;
+              if (docs == null || docs.isEmpty) {
+                return const Center(
+                  child: Text('No groups'),
+                );
+              }
+              int i = 0;
+              return ListView.builder(
+                itemCount: docs.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  final group = Group.fromSnapshot(docs[index]);
+                  if (!RegExp(_searchText, caseSensitive: false)
+                      .hasMatch(group.name)) {
+                    i += 1;
+                    if (i == docs.length) {
+                      return const Center(
+                        child: Text('No groups found'),
+                      );
                     }
-                  });
+                    return const SizedBox.shrink();
+                  }
+                  return FutureBuilder(
+                      future: DatabaseService.getUserData(group.admin!),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CupertinoActivityIndicator();
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          final admin = snapshot.data!.username;
+                          group.admin = admin;
+                          return GroupTile(
+                              uuid: widget.uuid,
+                              group: group,
+                              isJoined: group.members!.contains(widget.uuid)
+                                  ? 1
+                                  : group.requests!.contains(widget.uuid)
+                                      ? 2
+                                      : 0);
+                        }
+                      });
+                },
+              );
             },
-          );
-        },
+          ),
+        ]),
       ),
     );
   }
