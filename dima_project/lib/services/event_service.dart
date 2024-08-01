90dcc4e55005211075b23faa444f79c3d079cfda
sergiolupo <sergio.lupo@mail.polimi.it>
2024-08-01 16:13:56 +0200

    Managed better location permission

--- dima_project/lib/services/event_service.dart
+++ dima_project/lib/services/event_service.dart
@@ -11,21 +11,21 @@ class EventService {
     // Check if location services are enabled
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled, handle accordingly
-      throw Exception('Location services are disabled.');
+      // Location services are not enabled
+      return const LatLng(45.46284, 9.187469);
     }
 
     // Check for location permissions
     LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
       permission = await Geolocator.requestPermission();
       if (permission != LocationPermission.whileInUse &&
           permission != LocationPermission.always) {
-        // Permission is denied, handle accordingly
-        throw Exception('Location permission is denied.');
+        // Permission is denied
+        return const LatLng(45.46284, 9.187469);
       }
     }
-
     // Get the current position
     Position position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
