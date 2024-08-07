0fd673305ffe2edfd6149a0541137c302d0521ba
sergiolupo <sergio.lupo@mail.polimi.it>
2024-08-07 21:49:55 +0200

    Change provider service

--- dima_project/lib/services/provider_service.dart
+++ dima_project/lib/services/provider_service.dart
@@ -15,50 +15,70 @@ final userProvider = FutureProvider.family<UserData, String>((ref, uuid) async {
 
 final followerProvider =
     FutureProvider.family<List<UserData>, String>((ref, uuid) async {
-  final followersSnapshot = await DatabaseService().getFollowersUser(uuid);
-  final followersList =
-      (followersSnapshot.data() as Map<String, dynamic>)['followers'];
+  try {
+    final followersSnapshot = await DatabaseService().getFollowersUser(uuid);
+    final followersList =
+        (followersSnapshot.data() as Map<String, dynamic>)['followers'];
 
-  final List<UserData> followersData = [];
-  for (var followerId in followersList) {
-    final userData = await DatabaseService().getUserData(followerId);
-    followersData.add(userData);
+    final List<UserData> followersData = [];
+    for (var followerId in followersList) {
+      final userData = await DatabaseService().getUserData(followerId);
+      followersData.add(userData);
+    }
+
+    return followersData;
+  } catch (e) {
+    return [];
   }
-
-  return followersData;
 });
 
 final followingProvider =
     FutureProvider.family<List<UserData>, String>((ref, uuid) async {
-  final followingSnapshot = await DatabaseService().getFollowersUser(uuid);
-  final followingList =
-      (followingSnapshot.data() as Map<String, dynamic>)['following'];
+  try {
+    final followingSnapshot = await DatabaseService().getFollowersUser(uuid);
+    final followingList =
+        (followingSnapshot.data() as Map<String, dynamic>)['following'];
 
-  final List<UserData> followingData = [];
-  for (var followingId in followingList) {
-    final userData = await DatabaseService().getUserData(followingId);
-    followingData.add(userData);
+    final List<UserData> followingData = [];
+    for (var followingId in followingList) {
+      final userData = await DatabaseService().getUserData(followingId);
+      followingData.add(userData);
+    }
+
+    return followingData;
+  } catch (e) {
+    return [];
   }
-
-  return followingData;
 });
 
 final groupsProvider =
     FutureProvider.family<List<Group>, String>((ref, uuid) async {
-  final groups = await DatabaseService().getGroups(uuid);
-  return groups;
+  try {
+    final groups = await DatabaseService().getGroups(uuid);
+    return groups;
+  } catch (e) {
+    return [];
+  }
 });
 
 final joinedEventsProvider =
     FutureProvider.family<List<Event>, String>((ref, uuid) async {
-  final events = await DatabaseService().getJoinedEvents(uuid);
-  return events;
+  try {
+    final events = await DatabaseService().getJoinedEvents(uuid);
+    return events;
+  } catch (e) {
+    return [];
+  }
 });
 
 final createdEventsProvider =
     FutureProvider.family<List<Event>, String>((ref, uuid) async {
-  final events = await DatabaseService().getCreatedEvents(uuid);
-  return events;
+  try {
+    final events = await DatabaseService().getCreatedEvents(uuid);
+    return events;
+  } catch (e) {
+    return [];
+  }
 });
 
 final eventProvider =
