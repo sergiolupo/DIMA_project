dff524c96a2123bd8615644ff051f5ed8a13a97d
sergiolupo <sergio.lupo@mail.polimi.it>
2024-08-18 02:13:07 +0200

    Add some tests

--- dima_project/test/integration_tests/user_profile_test.dart
+++ dima_project/test/integration_tests/user_profile_test.dart
@@ -6,6 +6,7 @@ import 'package:dima_project/pages/user_profile/user_profile_page.dart';
 import 'package:dima_project/pages/user_profile/user_profile_tablet_page.dart';
 import 'package:dima_project/services/auth_service.dart';
 import 'package:dima_project/services/provider_service.dart';
+import 'package:dima_project/utils/constants.dart';
 import 'package:dima_project/widgets/events/event_grid.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -20,12 +21,11 @@ import '../mocks/mock_notification_service.mocks.dart';
 void main() {
   late final MockDatabaseService mockDatabaseService;
   late final MockNotificationService mockNotificationService;
-
-  group("User profile test for mobile", () {
-    setUpAll(() {
-      mockDatabaseService = MockDatabaseService();
-      mockNotificationService = MockNotificationService();
-    });
+  setUpAll(() {
+    mockDatabaseService = MockDatabaseService();
+    mockNotificationService = MockNotificationService();
+  });
+  group("User profile test for mobile in light mode", () {
     testWidgets(
         "User profile of the current user renders correctly and navigations work for mobile layout",
         (WidgetTester tester) async {
@@ -288,6 +288,328 @@ void main() {
               user: 'uid1',
             ),
           )),
+        ),
+      );
+      await tester.pumpAndSettle();
+      expect(find.text("username1"), findsOneWidget);
+      expect(find.text("name1 surname1"), findsOneWidget);
+      expect(find.text("This Account is private"), findsOneWidget);
+      expect(find.text("Follow"), findsOneWidget);
+      await tester.tap(find.text("Groups"));
+      await tester.pumpAndSettle();
+      expect(find.text("No groups"), findsOneWidget);
+      await tester.tap(find.byIcon(CupertinoIcons.back));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text("Followers"));
+      await tester.pumpAndSettle();
+      expect(find.text("No followers"), findsOneWidget);
+      await tester.tap(find.byIcon(CupertinoIcons.back));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text("Following"));
+      await tester.pumpAndSettle();
+      expect(find.text("Not following anyone"), findsOneWidget);
+      await tester.tap(find.byIcon(CupertinoIcons.back));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text("Follow"));
+      await tester.pumpAndSettle();
+      await tester.tap(find.byIcon(FontAwesomeIcons.envelope));
+      await tester.pumpAndSettle();
+      expect(find.text("username1"), findsOneWidget);
+    });
+  });
+
+  group("User profile test for mobile in dark mode", () {
+    testWidgets(
+        "User profile of the current user renders correctly and navigations work for mobile layout in dark mode",
+        (WidgetTester tester) async {
+      AuthService.setUid('test');
+      await tester.pumpWidget(
+        ProviderScope(
+          overrides: [
+            notificationServiceProvider
+                .overrideWithValue(mockNotificationService),
+            userProvider.overrideWith(
+              (ref, uid) => Future.value(UserData(
+                  uid: 'test',
+                  email: 'mail',
+                  username: 'username',
+                  imagePath: '',
+                  categories: ['Sports'],
+                  name: 'name',
+                  surname: 'surname')),
+            ),
+            followerProvider.overrideWith(
+              (ref, uid) => Future.value([
+                UserData(
+                    uid: 'uid1',
+                    email: 'mail1',
+                    username: 'username1',
+                    imagePath: '',
+                    categories: ['Sports'],
+                    name: 'name1',
+                    surname: 'surname1')
+              ]),
+            ),
+            followingProvider.overrideWith(
+              (ref, uid) => Future.value([
+                UserData(
+                    uid: 'uid2',
+                    email: 'mail2',
+                    username: 'username2',
+                    imagePath: '',
+                    categories: ['Sports'],
+                    name: 'name2',
+                    surname: 'surname2')
+              ]),
+            ),
+            databaseServiceProvider.overrideWithValue(mockDatabaseService),
+            groupsProvider.overrideWith(
+              (ref, uid) => Future.value([
+                Group(
+                  name: "group name",
+                  id: "id",
+                  imagePath: '',
+                  isPublic: true,
+                  members: ['test'],
+                )
+              ]),
+            ),
+            joinedEventsProvider.overrideWith(
+              (ref, id) => Future.value([
+                Event(
+                  id: '321',
+                  imagePath: '',
+                  admin: 'uid1',
+                  name: 'event name',
+                  description: 'description',
+                  isPublic: true,
+                  details: [
+                    EventDetails(
+                        startDate: DateTime(2024, 3, 2, 1, 1, 1),
+                        startTime: DateTime(2024, 3, 2, 1, 1, 1),
+                        endDate: DateTime(2025, 3, 2, 1, 1, 1),
+                        endTime: DateTime(2025, 3, 2, 1, 1, 1),
+                        location: 'Location',
+                        latlng: const LatLng(0, 0),
+                        id: 'id',
+                        members: ['test', 'uid1'],
+                        requests: [])
+                  ],
+                ),
+              ]),
+            ),
+            createdEventsProvider.overrideWith(
+              (ref, id) => Future.value([
+                Event(
+                  id: '123',
+                  imagePath: '',
+                  admin: 'test',
+                  name: 'name',
+                  description: 'description',
+                  isPublic: true,
+                  details: [
+                    EventDetails(
+                        startDate: DateTime(2024, 3, 2, 1, 1, 1),
+                        startTime: DateTime(2024, 3, 2, 1, 1, 1),
+                        endDate: DateTime(2025, 3, 2, 1, 1, 1),
+                        endTime: DateTime(2025, 3, 2, 1, 1, 1),
+                        location: 'Location',
+                        latlng: const LatLng(0, 0),
+                        id: 'id',
+                        members: ['test'],
+                        requests: [])
+                  ],
+                ),
+              ]),
+            ),
+            eventProvider.overrideWith(
+              (ref, id) => Future.value(
+                Event(
+                  id: '123',
+                  imagePath: '',
+                  admin: 'test',
+                  name: 'name',
+                  description: 'description',
+                  isPublic: true,
+                  details: [
+                    EventDetails(
+                        startDate: DateTime(2024, 3, 2, 1, 1, 1),
+                        startTime: DateTime(2024, 3, 2, 1, 1, 1),
+                        endDate: DateTime(2025, 3, 2, 1, 1, 1),
+                        endTime: DateTime(2025, 3, 2, 1, 1, 1),
+                        location: 'Location',
+                        latlng: const LatLng(0, 0),
+                        id: 'id',
+                        members: ['test'],
+                        requests: [])
+                  ],
+                ),
+              ),
+            ),
+          ],
+          child: MediaQuery(
+            data: const MediaQueryData(
+              platformBrightness: Brightness.dark,
+              size: Size(800.0, 600.0),
+            ),
+            child: CupertinoApp(
+                theme: const CupertinoThemeData(
+                  brightness: Brightness.dark,
+                ),
+                home: ResponsiveLayout(
+                  mobileLayout: UserProfile(
+                    user: AuthService.uid,
+                  ),
+                  tabletLayout: UserProfileTablet(
+                    user: AuthService.uid,
+                  ),
+                )),
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
+      expect(find.text("username"), findsOneWidget);
+      expect(find.text("name surname"), findsOneWidget);
+      expect(find.text("Followers"), findsOneWidget);
+      expect(find.text("Following"), findsOneWidget);
+      expect(find.text("Groups"), findsOneWidget);
+      expect(find.text('1'), findsNWidgets(3));
+      expect(find.text("Sports"), findsOneWidget);
+      expect(find.text("Events created"), findsOneWidget);
+      expect(find.text("Events joined"), findsOneWidget);
+      await tester.tap(find.byIcon(CupertinoIcons.bars));
+      await tester.pumpAndSettle();
+      expect(find.text("Options"), findsOneWidget);
+      await tester.tap(find.byIcon(CupertinoIcons.back));
+      await tester.pumpAndSettle();
+      expect(find.text('username'), findsOneWidget);
+      expect(find.text('name surname'), findsOneWidget);
+      expect(find.text('Followers'), findsOneWidget);
+      expect(find.text('Following'), findsOneWidget);
+      expect(find.text('Groups'), findsOneWidget);
+      expect(find.text('1'), findsNWidgets(3));
+      expect(find.text("Events created"), findsOneWidget);
+      expect(find.text("Events joined"), findsOneWidget);
+      await tester.tap(find.text("Groups"));
+      await tester.pumpAndSettle();
+      expect(find.text("group name"), findsOneWidget);
+      expect(find.byType(Image), findsOneWidget);
+      await tester.enterText(find.byType(CupertinoSearchTextField), "ss");
+      await tester.pumpAndSettle();
+      expect(find.text('No groups found'), findsOneWidget);
+      await tester.tap(find.byIcon(CupertinoIcons.back));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text("Followers"));
+      await tester.pumpAndSettle();
+      expect(find.text('username1'), findsOneWidget);
+      expect(find.text('name1 surname1'), findsOneWidget);
+      expect(find.byType(Image), findsOneWidget);
+      await tester.enterText(find.byType(CupertinoSearchTextField), "ss");
+      await tester.pumpAndSettle();
+      expect(find.text('No followers found'), findsOneWidget);
+      await tester.tap(find.byIcon(CupertinoIcons.back));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text("Following"));
+      await tester.pumpAndSettle();
+      expect(find.text('username2'), findsOneWidget);
+      expect(find.text('name2 surname2'), findsOneWidget);
+      expect(find.byType(Image), findsOneWidget);
+      await tester.enterText(find.byType(CupertinoSearchTextField), "ss");
+      await tester.pumpAndSettle();
+      expect(find.text('Not following found'), findsOneWidget);
+      await tester.tap(find.byIcon(CupertinoIcons.back));
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(EventGrid));
+      await tester.pumpAndSettle();
+      expect(find.text('name'), findsOneWidget);
+      expect(find.text('description'), findsOneWidget);
+      await tester.tap(find.text("Go to Event"));
+      await tester.pumpAndSettle();
+      expect(find.text('name'), findsOneWidget);
+      expect(find.text('description'), findsOneWidget);
+      await tester.tap(find.byType(CupertinoNavigationBarBackButton));
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(CupertinoNavigationBarBackButton));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text("Events joined"));
+      await tester.pumpAndSettle();
+      await tester.tap(find.byType(EventGrid));
+      await tester.pumpAndSettle();
+      expect(find.text('event name'), findsOneWidget);
+      expect(find.text('description'), findsOneWidget);
+    });
+    testWidgets(
+        "User profile of another user renders correctly and navigations work for mobile layout in dark mode",
+        (WidgetTester tester) async {
+      AuthService.setUid('test');
+      when(mockDatabaseService.getPrivateChatIdFromMembers(any))
+          .thenAnswer((_) => Stream.value(null));
+      when(mockDatabaseService.getPrivateChats(any))
+          .thenAnswer((_) => Stream.value([]));
+      await tester.pumpWidget(
+        ProviderScope(
+          overrides: [
+            notificationServiceProvider
+                .overrideWithValue(mockNotificationService),
+            userProvider.overrideWith((ref, uid) => Future.value(
+                  UserData(
+                      requests: [],
+                      isPublic: false,
+                      uid: 'uid1',
+                      email: 'mail1',
+                      username: 'username1',
+                      imagePath: '',
+                      categories: ['Sports'],
+                      name: 'name1',
+                      surname: 'surname1'),
+                )),
+            followerProvider.overrideWith(
+              (ref, uid) => Future.value([]),
+            ),
+            followingProvider.overrideWith(
+              (ref, uid) => Future.value([]),
+            ),
+            databaseServiceProvider.overrideWithValue(mockDatabaseService),
+            groupsProvider.overrideWith(
+              (ref, uid) => Future.value([]),
+            ),
+            joinedEventsProvider.overrideWith(
+              (ref, id) => Future.value([]),
+            ),
+            createdEventsProvider.overrideWith(
+              (ref, id) => Future.value([]),
+            ),
+          ],
+          child: const MediaQuery(
+            data: MediaQueryData(
+              platformBrightness: Brightness.dark,
+              size: Size(800.0, 600.0),
+            ),
+            child: CupertinoApp(
+                theme: CupertinoThemeData(
+                  brightness: Brightness.dark,
+                  primaryColor: Constants.primaryColorDark,
+                  primaryContrastingColor:
+                      Constants.primaryContrastingColorDark,
+                  scaffoldBackgroundColor:
+                      Constants.scaffoldBackgroundColorDark,
+                  barBackgroundColor: Constants.barBackgroundColorDark,
+                  textTheme: CupertinoTextThemeData(
+                    textStyle: TextStyle(
+                      color: Constants.textColorDark,
+                    ),
+                  ),
+                ),
+                home: ResponsiveLayout(
+                  mobileLayout: UserProfile(
+                    user: 'uid1',
+                  ),
+                  tabletLayout: UserProfileTablet(
+                    user: 'uid1',
+                  ),
+                )),
+          ),
         ),
       );
       await tester.pumpAndSettle();
