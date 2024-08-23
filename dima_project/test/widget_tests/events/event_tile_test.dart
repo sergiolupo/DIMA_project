f5b59afbb8e6f3491e0bd29115bf0d9c3b41d406
sergiolupo <sergio.lupo@mail.polimi.it>
2024-08-23 16:58:31 +0200

    Add some tests

--- dima_project/test/widget_tests/events/event_tile_test.dart
+++ dima_project/test/widget_tests/events/event_tile_test.dart
@@ -1,23 +1,48 @@
+import 'package:dima_project/pages/events/event_page.dart';
 import 'package:dima_project/services/auth_service.dart';
+import 'package:dima_project/services/provider_service.dart';
 import 'package:dima_project/widgets/events/event_tile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dima_project/models/event.dart';
+import 'package:latlong2/latlong.dart';
+
+import '../../mocks/mock_database_service.mocks.dart';
+import '../../mocks/mock_notification_service.mocks.dart';
 
 void main() {
   final Event testEvent = Event(
-    id: '123',
-    name: 'Event',
-    description: 'Description',
-    imagePath: '',
-    admin: 'uid',
-    isPublic: true,
-  );
-  testWidgets('EventTile displays event name and description',
+      id: '123',
+      name: 'Event',
+      description: 'Description',
+      imagePath: '',
+      admin: 'uid',
+      isPublic: true,
+      details: [
+        EventDetails(
+          startDate: DateTime.now(),
+          endDate: DateTime.now(),
+          startTime: DateTime.now(),
+          endTime: DateTime.now(),
+          location: 'Location',
+          latlng: const LatLng(0, 0),
+          members: ['uid'],
+        ),
+      ]);
+  testWidgets(
+      'EventTile displays event name and description correctly and navigates on tap',
       (WidgetTester tester) async {
     AuthService.setUid('uid');
     await tester.pumpWidget(ProviderScope(
+      overrides: [
+        eventProvider.overrideWith(
+          (ref, eventId) => testEvent,
+        ),
+        databaseServiceProvider.overrideWithValue(MockDatabaseService()),
+        notificationServiceProvider
+            .overrideWithValue(MockNotificationService()),
+      ],
       child: CupertinoApp(
         home: CupertinoPageScaffold(
           child: EventTile(event: testEvent),
@@ -27,5 +52,10 @@ void main() {
 
     expect(find.text('Event'), findsOneWidget);
     expect(find.text('Description: Description'), findsOneWidget);
+
+    await tester.tap(find.text('Event'));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(EventPage), findsOneWidget);
   });
 }
--- dima_project/test/widget_tests/news/category_tile_test.dart
+++ dima_project/test/widget_tests/news/category_tile_test.dart
@@ -1,6 +1,8 @@
+import 'package:dima_project/pages/news/category_news.dart';
 import 'package:dima_project/widgets/news/category_tile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import '../../mocks/mock_database_service.mocks.dart';
 import '../../mocks/mock_news_service.mocks.dart';
@@ -11,6 +13,8 @@ void main() {
       (WidgetTester tester) async {
     const categoryName = 'Cooking';
     const image = 'assets/categories/cooking.jpg';
+    final MockNewsService newsService = MockNewsService();
+    when(newsService.categories).thenReturn([]);
     TestWidgetsFlutterBinding.ensureInitialized();
     await tester.pumpWidget(
       CupertinoApp(
@@ -19,7 +23,7 @@ void main() {
             CategoryTile(
               image: image,
               categoryName: categoryName,
-              newsService: MockNewsService(),
+              newsService: newsService,
               databaseService: MockDatabaseService(),
             ),
           ],
@@ -27,8 +31,12 @@ void main() {
       ),
     );
 
-    // Verify that the CategoryTile displays the correct information
     expect(find.text(categoryName), findsOneWidget);
     expect(find.byType(Image), findsOneWidget);
+
+    await tester.tap(find.text(categoryName));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(CategoryNews), findsOneWidget);
   });
 }
